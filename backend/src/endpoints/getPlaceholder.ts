c710406bcaa1e214441e766e258f5b7f20e51553
Eve Keinan <evavkein@gmail.com>
2026-03-17 17:39:38 +0200

    fix lint error in getPlaceholder.ts

--- backend/src/endpoints/getPlaceholder.ts
+++ backend/src/endpoints/getPlaceholder.ts
@@ -1,8 +1,8 @@
 import {type Express} from "express";
-import {type Pool} from "pg";
+// import {type Pool} from "pg";
 import {timestampedLog} from "../logging.js";
 
-export function getPlaceholder(app: Express, db: Pool) {
+export function getPlaceholder(app: Express /*, db: Pool*/) {
 	app.get("/api/placeholder", async (req, res) => {
 		timestampedLog("Received request to " + req.baseUrl);
 		res.status(200).json();
