26eafba1ee52f47e53488d28a47c7b76f72bd377
Eve Keinan <evavkein@gmail.com>
2026-03-24 10:22:08 +0200

    add configurations for absolute-path imports in frontend, backend, and shared

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,15 @@
 import {defineConfig} from "vite";
 import react from "@vitejs/plugin-react";
+import path from "node:path";
 
 // https://vite.dev/config/
 export default defineConfig({
+	resolve: {
+		alias: {
+			"#": path.resolve(__dirname, "./"),
+			"#shared": path.resolve(__dirname, "../shared"),
+		},
+	},
 	plugins: [
 		react({
 			babel: {
