6e3debecf2466e06516c3922dcc8ac8d6c5b2463
ltaalas <ltaalas@student.hive.fi>
2026-04-16 18:16:38 +0300

    Adds ability to remove files from file upload list, Changes behaviour on error to not reset the upload list, Adds ability to append to upload list

--- frontend/src/dashboard/FileUpload.tsx
+++ frontend/src/dashboard/FileUpload.tsx
@@ -3,14 +3,20 @@ import {Button} from "../components/Button";
 import {useShowToast} from "../components/toastStore.ts";
 
 function FileUploader({refreshFileList}: {refreshFileList: () => void}) {
-	const [fileUploads, setFileUploads] = useState<FileList | null>(null);
+	const [fileUploads, setFileUploads] = useState<Array<File> | null>(null);
 	const fileInputRef = useRef<HTMLInputElement>(null);
 	const MAX_FILE_SIZE = 1000000; // 1meg
 	const showToast = useShowToast();
 
-	function handleFileChange(e: React.ChangeEvent<HTMLInputElement>) {
+	function resetInput() {
+		setFileUploads(null);
+		if (fileInputRef.current) fileInputRef.current.value = "";
+	}
+
+	async function handleFileChange(e: React.ChangeEvent<HTMLInputElement>) {
 		if (!e.target.files) return;
 
+		const newFileArray = fileUploads?.slice() ?? new Array<File>();
 		for (const f of e.target.files) {
 			if (f.size > MAX_FILE_SIZE) {
 				showToast("error", `File '${f.name}' too large at ${f.size} (max. ${MAX_FILE_SIZE})`);
@@ -18,9 +24,19 @@ function FileUploader({refreshFileList}: {refreshFileList: () => void}) {
 				setFileUploads(null);
 				return;
 			}
+			newFileArray.push(f);
 		}
 
-		setFileUploads(e.target.files);
+		setFileUploads(newFileArray);
+		console.log(fileUploads);
+	}
+
+	async function handleRemove(name: string) {
+		if (fileUploads === null) return;
+		const newFileArray = fileUploads.filter((value: File) => value.name !== name);
+		if (newFileArray.length) setFileUploads(newFileArray);
+		else setFileUploads(null);
+		// else console.log("trying to remove nonexistent file", name);
 	}
 
 	async function handleUpload() {
@@ -28,7 +44,7 @@ function FileUploader({refreshFileList}: {refreshFileList: () => void}) {
 			console.log("Uploading file(s)...", fileUploads);
 
 			const formData = new FormData();
-			[...fileUploads].forEach(function (file: File) {
+			[...fileUploads].forEach((file: File) => {
 				formData.append("file", file);
 			});
 
@@ -42,14 +58,18 @@ function FileUploader({refreshFileList}: {refreshFileList: () => void}) {
 					const res = await result.json();
 					console.error(`${res.error.detail}`);
 					showToast("error", `${res.error.detail}`);
-					setFileUploads(null);
 					return;
 				}
 
+				if (!result.ok) {
+					showToast("error", `File creation failed! UNKNOWN REASON`);
+					console.error(`File creation failed! UNKNOWN REASON`);
+					resetInput();
+					return;
+				}
 				const data = await result.json();
 				refreshFileList();
-				setFileUploads(null);
-				if (fileInputRef.current) fileInputRef.current.value = "";
+				resetInput();
 				console.log(data);
 			} catch (error) {
 				console.error(error);
@@ -81,6 +101,9 @@ function FileUploader({refreshFileList}: {refreshFileList: () => void}) {
 								[...fileUploads].map((file) => (
 									<tr key={file.name}>
 										<td>🗎 {file.name}</td>
+										<td className="text-center">
+											<Button onClick={() => handleRemove(file.name)}> ☒ </Button>
+										</td>
 									</tr>
 								))}
 						</tbody>
