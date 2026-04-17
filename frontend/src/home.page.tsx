32b03dc1765934d08d3e3e3a378aac7ba95a016c
Eve Keinan <evavkein@gmail.com>
2026-04-17 11:32:17 +0300

    Remove file browser link from home page content

--- frontend/src/home.page.tsx
+++ frontend/src/home.page.tsx
@@ -1,14 +1,5 @@
 "use client";
 
-import {Link} from "react-router";
-
 export default function HomePage() {
-	return (
-		<div>
-			Transcendence: It's a thing we're making™
-			<div>
-				<Link to="/filebrowser">File Browser</Link>
-			</div>
-		</div>
-	);
+	return <div>Transcendence: It's a thing we're making™</div>;
 }
