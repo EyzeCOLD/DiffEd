94ef19e5950c8cc4ee3cd76ef569c25b464a22b8
Eve Keinan <evavkein@gmail.com>
2026-04-24 13:38:19 +0300

    Fix file-creation nav to session

--- frontend/src/fileBrowser/NewFile.tsx
+++ frontend/src/fileBrowser/NewFile.tsx
@@ -4,7 +4,6 @@ import {Button} from "#/src/components/Button";
 import {useShowToast} from "#/src/layout/toastStore.ts";
 import {Input} from "#/src/components/Input";
 import {apiFetch} from "#/src/utils.ts";
-import type {ApiResponse} from "#shared/src/types.ts";
 
 function NewFile() {
 	const [newFilename, setNewFilename] = useState<string>();
@@ -13,21 +12,30 @@ function NewFile() {
 	const showToast = useShowToast();
 
 	async function openNewFile() {
-		const result: ApiResponse<string> = await apiFetch("/api/files", {
+		const fileResult = await apiFetch<string>("/api/files", {
 			method: "POST",
 			body: JSON.stringify({name: newFilename}),
 			headers: [["Content-Type", "application/json"] as [string, string]],
 		});
 
-		if (!result.ok) {
-			console.error("File creation failed!");
+		if (!fileResult.ok) {
 			showToast("error", "File creation failed!");
 			return;
 		}
 
-		console.log("New file created!");
+		const workspaceResult = await apiFetch<{sessionId: string}>("/api/workspace", {
+			method: "POST",
+			body: JSON.stringify({fileId: fileResult.data}),
+			headers: [["Content-Type", "application/json"] as [string, string]],
+		});
+
+		if (!workspaceResult.ok) {
+			showToast("error", "Failed to open workspace");
+			return;
+		}
+
 		showToast("success", "New file created!");
-		if (result) navigate(`/collab/${result.data}`);
+		navigate(`/collab/${workspaceResult.data.sessionId}`);
 	}
 
 	return (
