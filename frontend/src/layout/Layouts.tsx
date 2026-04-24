86bf2584b75de7b3c31d1a6ceb9096d8646d21d5
Eve Keinan <evavkein@gmail.com>
2026-04-24 14:55:08 +0300

    Auto-login the user even on public layout

--- frontend/src/layout/Layouts.tsx
+++ frontend/src/layout/Layouts.tsx
@@ -5,7 +5,20 @@ import {getSession, apiFetch} from "../utils";
 import {useCurrentUser, useSetUser, useClearUser} from "../stores/userStore";
 import type {User} from "#shared/src/types.js";
 
+async function fetchAndSetUser(setUser: (u: User) => void): Promise<boolean> {
+	const res = await apiFetch<User>("/api/user", {method: "GET", credentials: "include"});
+	if (res.ok) setUser(res.data);
+	return res.ok;
+}
+
 export function PublicLayout() {
+	const storeUser = useCurrentUser();
+	const setUser = useSetUser();
+
+	useEffect(() => {
+		if (!storeUser) fetchAndSetUser(setUser);
+	}, []);
+
 	return (
 		<>
 			<PublicTopBar />
@@ -33,13 +46,9 @@ export function UserLayout() {
 				}
 			});
 		} else {
-			apiFetch<User>("/api/user", {method: "GET", credentials: "include"}).then((res) => {
-				if (!res.ok) {
-					navigate("/login", {replace: true});
-				} else {
-					setUser(res.data);
-					setReady(true);
-				}
+			fetchAndSetUser(setUser).then((ok) => {
+				if (!ok) navigate("/login", {replace: true});
+				else setReady(true);
 			});
 		}
 	}, []);
