da7bbfcbd4c4ad81046612a7a009864d46e622c9
Eve Keinan <evavkein@gmail.com>
2026-05-07 15:38:34 +0300

    OAuth readability improvements

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -41,8 +41,8 @@ UserEndpoints.getUser(app);
 SessionEndpoints.loginUser(app);
 SessionEndpoints.logoutUser(app);
 SessionEndpoints.getSession(app);
-OAuthEndpoints.githubAuth(app);
-OAuthEndpoints.githubCallback(app);
+OAuthEndpoints.githubAuthStart(app);
+OAuthEndpoints.githubAuthCallback(app);
 OAuthEndpoints.setGithubUsername(app);
 OAuthEndpoints.githubUnlink(app);
 
