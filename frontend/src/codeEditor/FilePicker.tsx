3f9c9883de92e867876f6573e67f2a0251d3518b
ltaalas <ltaalas@student.hive.fi>
2026-05-12 00:57:18 +0300

    screwing with getting the filelist to refresh in annoying places

--- frontend/src/codeEditor/FilePicker.tsx
+++ frontend/src/codeEditor/FilePicker.tsx
@@ -1,14 +1,26 @@
 import {type JSX} from "react";
 import {CollabConnection, pickFile} from "./collabClient";
 import FileBrowser from "../fileBrowser/FileBrowser";
+import {useShowToast} from "../stores/toastStore";
+import useFileBrowser from "../fileBrowser/useFileBrowser";
 
 type FilePickerProps = {
 	connection: CollabConnection;
 };
 
 function FilePicker({connection}: FilePickerProps): JSX.Element {
+	const showtoast = useShowToast();
+	const {refreshFileList} = useFileBrowser();
+
 	async function handlePick(fileId: string) {
-		await pickFile(connection, fileId);
+		try {
+			await pickFile(connection, fileId);
+		} catch (err) {
+			const error: Error = err as Error;
+			console.log(error.message);
+			showtoast("error", error.message);
+			refreshFileList();
+		}
 	}
 
 	return (
